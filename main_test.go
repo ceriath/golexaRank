219e7e2ee14e616769648fe3e881801b185df6cd
qwer7y <suhasmallya97@gmail.com>
2018-12-18 12:28:40 +0530

    Added tests for the sign function and getSignatureKey function

--- main_test.go
+++ main_test.go
@@ -10,17 +10,27 @@ func TestCreateV4Signature(t *testing.T) {
 }
 
 func TestSign(t *testing.T) {
-	key := []byte("Sucker")
+	key := []byte("Some fucking test case string")
 	dateStamp := []byte("20120215")
 	testDrive := sign(key, dateStamp)
-	correctValue := "2dd290c65716bf297ee9feb426fc708002c6ace2dace50b925e93dee5a9141c9"
-	if hex.EncodeToString(testDrive) != correctValue {
-		t.Error("The hash values don't match")
+	calculatedValue := hex.EncodeToString(testDrive)
+	correctValue := "f2f077be4009e87c842f68c51f42600352d63ea696e97be5e8a2aef51fa5168b"
+	if calculatedValue != correctValue {
+		t.Error("")
 	}
 }
 
 func TestGetSignatureKey(t *testing.T) {
-
+	key := "Some fucking key string"
+	ds := "Some stupid datestamp"
+	regionName := "Some idiotic region name"
+	serviceName := "Blah blah blah"
+	calculatedValue := getSignatureKey(key, ds, regionName, serviceName)
+	calculatedHexValue := hex.EncodeToString(calculatedValue)
+	correctValue := "d66455cc65b63e63c7efb9341602ff784d8f68c3f36ecaac4a783ba1d2ddc280"
+	if calculatedHexValue != correctValue {
+		t.Error("")
+	}
 }
 
 func TestGetUrlInfo(t *testing.T) {
@@ -38,9 +48,5 @@ func TestGetCategoryBrowseInformation(t *testing.T) {
 }
 
 func TestReturnOutput(t *testing.T) {
-
-}
-
-func TestName(t *testing.T) {
 
 }
