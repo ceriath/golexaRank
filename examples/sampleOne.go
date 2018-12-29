356a40de036d6d42e036122db6a1eb31aac4ca15
qwer7y <suhasmallya97@gmail.com>
2018-12-29 13:43:39 +0530

    Accomodated the changes in the library

--- examples/sampleOne.go
+++ examples/sampleOne.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/qwer7y/golexaRank"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
@@ -14,8 +16,19 @@ func main() {
 	sitesLinkingInResponseGroup := "SitesLinkingIn"
 	categoryBrowseInfoResponseGroup := "Categories%2CRelatedCategories%2CLanguageCategories%2CLetterBars"
 	exampleDomain := "www.github.com"
+
+	// Reading the credentials required for issuing the requests
+	fileReadBytes, err := ioutil.ReadFile("credentials.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileReadString := string(fileReadBytes)
+	fileReadStringSplit := strings.Split(fileReadString, "\n")
+	accessID := fileReadStringSplit[0]
+	secretAccessKey := fileReadStringSplit[1]
+
 	// Let's see if the GetUrlInfo function works
-	response := golexaRank.GetUrlInfo(exampleDomain, urlInfoResponseGroups)
+	response := golexaRank.GetUrlInfo(exampleDomain, urlInfoResponseGroups, accessID, secretAccessKey)
 	if response.StatusCode != 200 {
 		println("Response status code: " + response.Status)
 		println("Response headers: ")
