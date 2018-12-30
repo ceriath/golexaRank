3c57c11a7105dc34a4b7b743ba7a8b8b074d49a4
qwer7y <suhasmallya97@gmail.com>
2018-12-30 11:31:56 +0530

    Divided up the examples

---
+++ examples/getSitesLinkingInExample.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/qwer7y/golexaRank"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	// Check for the Python urlencode equivalent of this
+	sitesLinkingInResponseGroup := "SitesLinkingIn"
+	exampleDomain := "www.github.com"
+
+	// Reading the credentials required for issuing the requests
+	fileReadBytes, err := ioutil.ReadFile("credentials.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileReadString := string(fileReadBytes)
+	fileReadStringSplit := strings.Split(fileReadString, "\n")
+	accessID := fileReadStringSplit[0]
+	secretAccessKey := fileReadStringSplit[1]
+
+	// Let's see if the sitesLinkingIn function works
+	response := golexaRank.GetSitesLinkingIn(exampleDomain, sitesLinkingInResponseGroup, accessID, secretAccessKey)
+	if response.StatusCode != 200 {
+		println("Response status code: " + response.Status)
+		println("Response headers: ")
+		for key, value := range response.Header {
+			print(key + ": ")
+			for v := range value {
+				println(v)
+			}
+		}
+	}
+}
---
+++ examples/urlInfoExample.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/qwer7y/golexaRank"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	// Check for the Python urlencode equivalent of this
+	urlInfoResponseGroups := "RelatedLinks%2CCategories%2CRank%2CContactInfo%2CRankByCountry%2CUsageStats%2CSpeed%2CLanguage%2C" +
+		"OwnedDomains%2CLinksInCount%2CSiteData%2CAdultContent"
+	exampleDomain := "www.github.com"
+
+	// Reading the credentials required for issuing the requests
+	fileReadBytes, err := ioutil.ReadFile("credentials.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileReadString := string(fileReadBytes)
+	fileReadStringSplit := strings.Split(fileReadString, "\n")
+	accessID := fileReadStringSplit[0]
+	secretAccessKey := fileReadStringSplit[1]
+
+	// Let's see if the GetUrlInfo function works
+	response := golexaRank.GetUrlInfo(exampleDomain, urlInfoResponseGroups, accessID, secretAccessKey)
+	if response.StatusCode != 200 {
+		println("Response status code: " + response.Status)
+		println("Response headers: ")
+		//for key, value := range response.Header {
+		//	print(key + ": ")
+		//	for v := range value {
+		//		println(v)
+		//	}
+		//}
+		buf := new(bytes.Buffer)
+		_, err := buf.ReadFrom(response.Body)
+		if err != nil {
+			println("Problem reading response.Body")
+		}
+		newStr := buf.String()
+		fmt.Printf(newStr)
+	} else {
+		println("Success!")
+	}
+}
